26b826d828c2935f18232dd917efafe1ba294d4f
Mr. Robot <shubhampandeylearning@gmail.com>
2026-01-30 09:57:27 +0000

    Project order changed as perr priority

--- src/Components/ProjectSection.jsx
+++ src/Components/ProjectSection.jsx
@@ -4,6 +4,37 @@ const ProjectSection = () => {
     return (
         <div className="section w-screen grid grid-cols-3 gap-y-8 place-items-center
             ">
+
+            <ProjCards img={"effiya.png"}
+                       name={"PolyChat"}
+                       workingLink={""}
+                       source={""}
+                       category={"Web"}
+                       skills={["MERN","ReactJs","NodeJs","Styled Components","SocketIO","TensorFlowJs","Machine Learning"]}
+                       description=
+                           {"PolyChat is a multilingual web chat platform that uses machine learning models to perform real-time message translation, enabling seamless conversations between users speaking different languages without manual intervention."}
+            />
+
+            <ProjCards img={"effiya.png"}
+                       name={"ytPlaylist-Downloader"}
+                       workingLink={""}
+                       source={""}
+                       category={"Terminal/CLI"}
+                       skills={["Python","Automation"]}
+                       description=
+                           {"A terminal-based YouTube downloader designed for bulk efficiency, enabling one-command downloads of full playlists—solving a common limitation of existing tools that require manual, per-video downloads."}
+            />
+
+            <ProjCards img={"effiya.png"}
+                       name={"KeyFortress"}
+                       workingLink={""}
+                       source={""}
+                       category={"Android"}
+                       skills={["Java","Firebase","XML","Picasso","Biometrics"]}
+                       description=
+                           {"KeyFortress is a biometric-first password security solution developed at a hackathon, enabling users to log into accounts using biometrics while securely storing auto-generated, high-strength passwords that are nearly impossible to crack."}
+            />
+
             <ProjCards img={"effiya.png"}
                        name={"ConnectHub"}
                        workingLink={""}
@@ -12,17 +43,6 @@ const ProjectSection = () => {
                        skills={["Java","Firebase","Git","XML","Picasso",]}
                        description=
                            {"ConnectHub is a social media app built on Firebase, supporting full CRUD for posts, image uploads, realtime search, and user discovery. Developed using MVVM architecture and structured Git workflows for scalability and maintainability."}
-            />
-
-
-            <ProjCards img={"effiya.png"}
-                       name={"BlinkChat"}
-                       workingLink={""}
-                       source={""}
-                       category={"Android"}
-                       skills={["Java","Firebase","Git","XML","Glide",]}
-                       description=
-                           {"BlinkChat is a lightweight realtime chat app powered by Firebase, featuring secure authentication, instant messaging, and image sharing—built using FirebaseUI, Realtime Database, and Storage for a fast, seamless chat experience."}
             />
 
 
@@ -47,35 +67,20 @@ const ProjectSection = () => {
                            {"A CLI-based Git–GitHub automation script that creates a GitHub repository and links it to a local Git project in a single command—streamlining repository setup, reducing manual steps, and improving developer workflow efficiency."}
             />
 
-            <ProjCards img={"effiya.png"}
-                       name={"ytPlaylist-Downloader"}
-                       workingLink={""}
-                       source={""}
-                       category={"Terminal/CLI"}
-                       skills={["Python","Automation"]}
-                       description=
-                           {"A terminal-based YouTube downloader designed for bulk efficiency, enabling one-command downloads of full playlists—solving a common limitation of existing tools that require manual, per-video downloads."}
-            />
+            <div></div>
+
 
             <ProjCards img={"effiya.png"}
-                       name={"KeyFortress"}
+                       name={"BlinkChat"}
                        workingLink={""}
                        source={""}
                        category={"Android"}
-                       skills={["Java","Firebase","XML","Picasso","Biometrics"]}
+                       skills={["Java","Firebase","Git","XML","Glide",]}
                        description=
-                           {"KeyFortress is a biometric-first password security solution developed at a hackathon, enabling users to log into accounts using biometrics while securely storing auto-generated, high-strength passwords that are nearly impossible to crack."}
+                           {"BlinkChat is a lightweight realtime chat app powered by Firebase, featuring secure authentication, instant messaging, and image sharing—built using FirebaseUI, Realtime Database, and Storage for a fast, seamless chat experience."}
             />
 
-            <ProjCards img={"effiya.png"}
-                       name={"PolyChat"}
-                       workingLink={""}
-                       source={""}
-                       category={"Web"}
-                       skills={["MERN","ReactJs","NodeJs","Styled Components","SocketIO","TensorFlowJs","Machine Learning"]}
-                       description=
-                           {"PolyChat is a multilingual web chat platform that uses machine learning models to perform real-time message translation, enabling seamless conversations between users speaking different languages without manual intervention."}
-            />
+
         </div>
     )
 }
