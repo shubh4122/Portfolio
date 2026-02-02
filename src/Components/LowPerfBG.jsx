9cbd5a7d4cfa8aa40a5e60f363c4b2fad3de969d
Mr. Robot <shubhampandeylearning@gmail.com>
2026-02-02 00:36:03 +0000

    Try Fix 3 - static bg not loading

--- src/Components/LowPerfBG.jsx
+++ src/Components/LowPerfBG.jsx
@@ -1,7 +1,7 @@
 const LowPerfBG = () => {
     return (
         <div className="fixed bg-black z-[-2] w-screen h-screen">
-            <div className="fixed w-screen h-screen z-[-2] bg-[url(src/assets/staticBg.png)]">
+            <div className="fixed w-screen h-screen z-[-2] bg-[url(staticBg.png)]">
 
             </div>
         </div>
